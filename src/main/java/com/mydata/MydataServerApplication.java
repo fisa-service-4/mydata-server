9a560ba092cef098963cefa1cca3941a00ba93ee
yujung23 <kimyj4017@gmail.com>
2026-05-19 16:13:08 +0900

    chore(mydata): mydata 서버 초기 프로젝트 구조 구성

---
+++ src/main/java/com/mydata/MydataServerApplication.java
@@ -0,0 +1,13 @@
+package com.mydata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MydataServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MydataServerApplication.class, args);
+	}
+
+}
