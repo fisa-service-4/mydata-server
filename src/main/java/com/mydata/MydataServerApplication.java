011fc1b323a3af0ea8ab6c143fee777fd6ee916d
hyun7931 <119102748+hyun7931@users.noreply.github.com>
2026-05-23 10:55:38 +0900

    feat(infra): DockerFile, application-test.yaml 추가

--- src/main/java/com/mydata/MydataServerApplication.java
+++ src/main/java/com/mydata/MydataServerApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MydataServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MydataServerApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(MydataServerApplication.class, args);
+  }
 }
